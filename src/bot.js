e2d79a540e26b22e690e2fe66b43a87b1438c9f5
dobe4ever <132445266+dobe4ever@users.noreply.github.com>
2025-09-25 14:56:08 +0000

    f

--- src/bot.js
+++ src/bot.js
@@ -41,7 +41,7 @@ const aiConfigurations = {
 };
 
 let currentService = 'gemini';
-let currentModel = aiConfigurations.gemini.flash;
+let currentModel = aiConfigurations.gemini.pro;
 
 // /start
 bot.onText(/\/start/, (msg) => {
